dab8667c701ca016339768597d54760884d6a13c
David <dfp.ahmed@gmail.com>
2026-05-19 21:05:39 +0200

    fix: nom médicament reste noir même quand pris (v35)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v34';
+const CACHE = 'monsucre-v35';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
