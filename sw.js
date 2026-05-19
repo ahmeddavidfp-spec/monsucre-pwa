751b9f285361d6aa0b5d164a5412fefa88b19e6f
David <dfp.ahmed@gmail.com>
2026-05-19 20:48:34 +0200

    redesign: boutons appel sobre, liste verticale, grand et lisible

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v29';
+const CACHE = 'monsucre-v30';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
