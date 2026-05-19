420099193a816348664564feb33961f5eef288c9
David <dfp.ahmed@gmail.com>
2026-05-19 20:53:33 +0200

    fix: retour grille 2x2 pour les boutons appel, design sobre

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v31';
+const CACHE = 'monsucre-v32';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
