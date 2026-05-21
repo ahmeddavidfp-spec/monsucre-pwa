996da4e9898d8958a1e90058ed7959554731f7d4
David <dfp.ahmed@gmail.com>
2026-05-21 09:52:09 +0200

    ux: icones SVG sobres + gap boutons + suppression gradient, v93

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v92';
+const CACHE = 'monsucre-v93';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
