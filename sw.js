96df3999f0152d2364147b097054f69abaee71ef
David <dfp.ahmed@gmail.com>
2026-05-21 12:44:53 +0200

    glycemie: hint plus visible (vert gras), SW v106

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v105';
+const CACHE = 'monsucre-v106';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
