071c45994585a83c17cf2d37b4ec50c8ae01d984
David <dfp.ahmed@gmail.com>
2026-05-21 10:05:48 +0200

    ux: icone coeur SVG sobre sur bouton urgence, v99

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v98';
+const CACHE = 'monsucre-v99';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
