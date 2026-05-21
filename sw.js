b84847ae3fdf4d30bc3278984e7404b32e25944d
David <dfp.ahmed@gmail.com>
2026-05-21 12:50:16 +0200

    glycemie: hint integre dans la zone de saisie, SW v107

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v106';
+const CACHE = 'monsucre-v107';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
