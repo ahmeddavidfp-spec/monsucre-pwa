68c23746a457509af1290039f2f7ad2d4ed57f70
David <dfp.ahmed@gmail.com>
2026-05-22 11:10:51 +0200

    accueil: icone repas violet/indigo pour distinguer du bouton urgence, SW v119

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v118';
+const CACHE = 'monsucre-v119';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
