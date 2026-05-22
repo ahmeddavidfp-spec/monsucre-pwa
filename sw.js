a42a33d9826adeb0754f397260e695791d024236
David <dfp.ahmed@gmail.com>
2026-05-22 11:23:34 +0200

    accueil: icone repas violet mauve distinct, SW v120

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v119';
+const CACHE = 'monsucre-v120';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json'];
 
 self.addEventListener('install', e => {
