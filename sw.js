3f3a43b47638082fc6879c165ff34a743462dd7f
David <dfp.ahmed@gmail.com>
2026-05-24 08:05:39 +0200

    ux: bouton Enregistrer avant le hint, directement sous le champ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monsucre-v163';
+const CACHE = 'monsucre-v164';
 const ASSETS = ['/', '/index.html', '/public/style.css', '/public/app.js', '/public/manifest.json', '/public/medicaments-be.json'];
 
 self.addEventListener('install', e => {
